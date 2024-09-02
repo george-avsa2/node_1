89022a14b168220c96b6f3f0f31aa156ef75699c
george.avsa2 <georgerya0@gmail.com>
2024-09-02 14:27:11 +0200

    fix: added try catch; added case, when input string is invalid

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 var pjson = require("./package.json");
 
 function countReversePolishNotation(str) {
-  console.log(`Hello, current version is ${pjson.version}`);
-  const stack = [];
-  const lolkek = str.split(" ");
-  lolkek.forEach((lol) => {
-    const int = Number.parseInt(lol);
-    if (int || int === 0) {
-      stack.push(int);
-    } else {
-      const operand1 = stack.pop();
-      const operand2 = stack.pop();
+  try {
+    console.log(`Hello, current version is ${pjson.version}`);
+    const stack = [];
+    const splittedInputs = str.split(" ");
+    splittedInputs.forEach((splittedInput) => {
+      const int = Number.parseInt(splittedInput);
+      if (int || int === 0) {
+        stack.push(int);
+      } else {
+        const operand1 = stack.pop();
+        const operand2 = stack.pop();
 
-      if (lol === "*") {
-        stack.push(operand1 * operand2);
-      } else if (lol === "+") {
-        stack.push(operand1 + operand2);
-      } else if (lol === "/") {
-        stack.push(operand2 / operand1);
-      } else if (lol === "-") {
-        stack.push(operand2 - operand1);
+        if (splittedInput === "*") {
+          stack.push(operand1 * operand2);
+        } else if (splittedInput === "+") {
+          stack.push(operand1 + operand2);
+        } else if (splittedInput === "/") {
+          stack.push(operand2 / operand1);
+        } else if (splittedInput === "-") {
+          stack.push(operand2 - operand1);
+        }
       }
+    });
+
+    if (stack.length !== 1) {
+      throw new "Someting wrong with input string"();
     }
-  });
-  console.log(stack);
+
+    return stack[0];
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = countReversePolishNotation;
